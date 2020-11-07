12dfc9dac58082e3c21a6a3a72a2f0411ca04677
Julien <skelirphoteman@gmail.com>
2020-11-07 15:20:09 +0100

    setting web server environment
    Start web server for formation on Skelir's Creation web site.

---
+++ www/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Apprendre le PHP - Les bases</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<h1 align="center">Bonjour à toi ! </h1>
+		<div class="container">
+			<p align="center">Si tu as téléchargé cette environnement de travail 
+				c'est surement pour suivre la formation de Skelir's Creation sur <b>Apprendre le PHP - Les Bases</b>.
+				J'espère que ta formation se passera bien ! Si as la moindre question, contact moi en message
+				sur <a href="https://www.instagram.com/skelirs_creation/">Instagram</a></p>
+				<img src="https://skc-shop.fr/wp-content/uploads/2020/05/29793727_421311501662907_2281635147604295680_o-1024x662.png" class="rounded mx-auto d-block" alt="Skelir's Creation">
+		</div>
+	</div>
+</body>
+</html>
